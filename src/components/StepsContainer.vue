c86d702bf23d1a77890556a1b02c12e1dfb28d83
Tsotne <cotnemexuri@gmail.com>
2023-06-03 22:29:55 +0400

    Initial of steps container

---
+++ src/components/StepsContainer.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="steps-container">
+        <div class="step-head">
+            <h2>{{ title }}</h2>
+            <p>{{ description}}</p>
+        </div>
+        <slot />
+        <div class="next-prev-buttons">
+            <button class="prev">Go back</button>
+            <button class="next">Next Step</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StepsContainer",
+    props: {
+        title: {
+            type: String,
+            default: ""
+        },
+        description: {
+            type: String,
+            default: ""
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.steps-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 400px;
+    height: 510px;
+
+    .next-prev-buttons {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .prev {
+            color: #9B9BA3;
+            background-color: #FFFFFF;
+            font-family: ubuntu-medium, sans-serif;
+            font-size: 16px;
+        }
+
+        .next {
+            background-color: #03295A;
+            color: #FFFFFF;
+            padding: 12px 18px;
+            border-radius: 6px;
+            font-family: ubuntu-medium, sans-serif;
+            font-size: 16px;
+        }
+    }
+}
+</style>
