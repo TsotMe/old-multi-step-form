fbf4887289e3ee6a7a823a0f1a605056c5142eb4
Tsotne <cotnemexuri@gmail.com>
2023-06-11 16:47:27 +0400

    Add some styles and value update functionality on CustomInput.vue

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -4,7 +4,7 @@
             <label>{{ labelText }}</label>
             <span>{{ errorText }}</span>
         </div>
-        <input :type="type" :placeholder="placeholderText">
+        <input :type="type" :placeholder="placeholderText" v-model="value">
     </div>
 </template>
 
@@ -27,6 +27,21 @@ export default {
         type: {
             type: String,
             default: ''
+        },
+        modelValue: {
+            type: [String, Number],
+            default: null
+        }
+    },
+    emits: ['update:modelValue'],
+    computed: {
+        value: {
+            get() {
+                return this.modelValue
+            },
+            set(value) {
+                this.$emit('update:modelValue', value)
+            }
         }
     }
 }
@@ -41,19 +56,35 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
+        margin-bottom: 8px;
 
         label {
-
+            color: #00194F;
         }
 
         span {
-
+            color: #D2545F;
+            font-family: ubuntu-medium, sans-serif;
         }
     }
 
     input {
         width: 100%;
-        height: 38px;
+        height: 48px;
+        border-radius: 6px;
+        border: 1px solid #9B9BA3;
+        color: #081C3D;
+        padding-left: 10px;
+        font-family: ubuntu-medium, sans-serif;
+        font-size: 16px;
+    }
+
+    input:focus {
+        border: 1px solid #554F95;
+    }
+
+    input::placeholder {
+        color: #9B9BA3;
     }
 }
 </style>
