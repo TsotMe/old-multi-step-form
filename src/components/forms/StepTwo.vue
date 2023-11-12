6ef85f3102c0e6ed9362d95f6717f16b061af353
Tsotne <cotnemexuri@gmail.com>
2023-11-12 15:21:48 +0400

    Initial component StepTwo.vue

---
+++ src/components/forms/StepTwo.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="step-two">
+
+</div>
+</template>
+
+<script>
+import CustomToggle from "@/components/UI/CustomToggle.vue";
+
+export default {
+    name: "StepTwo",
+    components: {CustomToggle}
+}
+</script>
+
+<style scoped lang="scss">
+.step-two {
+
+}
+</style>
