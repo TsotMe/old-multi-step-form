8ef661c2430bf50d70701b1b42240a4edd80744e
Tsotne <cotnemexuri@gmail.com>
2023-11-12 15:22:07 +0400

    Initial component CustomToggle.vue

---
+++ src/components/UI/CustomToggle.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="custom-toggle">
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "CustomToggle"
+}
+</script>
+
+<style scoped lang="scss">
+@use '@/assets/css/variables' as v;
+
+.custom-toggle {
+    width: 30px;
+    height: 10px;
+    border-radius: 6px;
+    background-color: v.$Marine-blue;
+}
+</style>
