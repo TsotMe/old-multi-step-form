6d663b507ce7f5b97ab526d547add33512932110
Tsotne <cotnemexuri@gmail.com>
2023-11-14 09:05:33 +0400

    Import and use CustomToggle component on StepTwo.vue

--- src/components/forms/StepTwo.vue
+++ src/components/forms/StepTwo.vue
@@ -1,15 +1,30 @@
 <template>
 <div class="step-two">
-
+    <CustomToggle v-model="isYearly"/>
 </div>
 </template>
 
 <script>
 import CustomToggle from "@/components/UI/CustomToggle.vue";
+import {useStepTwoStore} from "@/stores/StepTwoStore";
 
 export default {
     name: "StepTwo",
-    components: {CustomToggle}
+    components: {CustomToggle},
+    setup() {
+        const stepTwoStore = useStepTwoStore()
+        return {stepTwoStore}
+    },
+    data() {
+        return {
+            isYearly: false
+        }
+    },
+    watch: {
+        isYearly(newVal) {
+            this.stepTwoStore.billingType = newVal ? 2 : 1
+        }
+    }
 }
 </script>
 
