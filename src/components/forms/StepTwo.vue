7ff0849565e38ae2e460f8afcf8b3cf5d20b9a61
Tsotne <cotnemexuri@gmail.com>
2023-12-26 19:04:51 +0400

    Update second step

--- src/components/forms/StepTwo.vue
+++ src/components/forms/StepTwo.vue
@@ -1,5 +1,28 @@
 <template>
 <div class="step-two">
+    <div class="plans-cont">
+        <PlanComponent
+            :planImage="iconArcade"
+            planTitle="Arcade"
+            :planInitialPrice="9"
+            :class="{active: selectedPlan === 1}"
+            @setSelectedPlan="selectedPlan = 1"
+        />
+        <PlanComponent
+            :planImage="iconAdvanced"
+            planTitle="Advanced"
+            :planInitialPrice="12"
+            :class="{active: selectedPlan === 2}"
+            @setSelectedPlan="selectedPlan = 2"
+        />
+        <PlanComponent
+            :planImage="iconPro"
+            planTitle="Pro"
+            :planInitialPrice="15"
+            :class="{active: selectedPlan === 3}"
+            @setSelectedPlan="selectedPlan = 3"
+        />
+    </div>
     <div class="billing-type-wrapper">
         <span :class="{active: stepTwoStore.billingType === 1}">Monthly</span>
         <CustomToggle v-model="isYearly"/>
@@ -11,17 +34,25 @@
 <script>
 import CustomToggle from "@/components/UI/CustomToggle.vue";
 import {useStepTwoStore} from "@/stores/StepTwoStore";
+import PlanComponent from "@/components/second-step/PlanComponent.vue";
+import iconAdvanced from "@/assets/images/icon-advanced.svg"
+import iconArcade from "@/assets/images/icon-arcade.svg"
+import iconPro from "@/assets/images/icon-pro.svg"
 
 export default {
     name: "StepTwo",
-    components: {CustomToggle},
+    components: {PlanComponent, CustomToggle},
     setup() {
         const stepTwoStore = useStepTwoStore()
         return {stepTwoStore}
     },
     data() {
         return {
-            isYearly: false
+            isYearly: false,
+            iconAdvanced: iconAdvanced,
+            iconArcade: iconArcade,
+            iconPro: iconPro,
+            selectedPlan: 1
         }
     },
     watch: {
@@ -36,6 +67,23 @@ export default {
 @use '@/assets/css/variables' as v;
 
 .step-two {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+
+    > .plans-cont {
+        display: grid;
+        align-items: center;
+        justify-content: space-between;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 16px;
+
+        > .active {
+            border-color: black;
+        }
+    }
+
     > .billing-type-wrapper {
         display: flex;
         align-items: center;
---
+++ src/components/second-step/PlanComponent.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="plan-component" @click="$emit('setSelectedPlan')">
+    <img :src="planImage" alt="">
+    <div class="plan-info-wrapper">
+        <h3>{{ planTitle }}</h3>
+        <span>{{ planPrice }}</span>
+        <span>2 months free</span>
+    </div>
+</div>
+</template>
+
+<script>
+import {useStepTwoStore} from "@/stores/StepTwoStore";
+
+export default {
+    name: "PlanComponent",
+    props: {
+        planImage: {
+            type: String,
+            default: ''
+        },
+        planTitle: {
+            type: String,
+            default: ''
+        },
+        planInitialPrice: {
+            type: Number,
+            default: null
+        }
+    },
+    setup() {
+        const stepTwoStore = useStepTwoStore()
+        return {stepTwoStore}
+    },
+    computed: {
+        planPrice() {
+            if (this.stepTwoStore.billingType === 2) {
+                return `$${this.planInitialPrice * 10}/yr`
+            } else {
+                return `$${this.planInitialPrice}/mo`
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.plan-component {
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    transition: .2s;
+    border: 1px solid white;
+
+    &:hover {
+        border-color: black;
+    }
+
+    > img {
+        width: 40px;
+        height: 40px;
+    }
+
+    > .plan-info-wrapper {
+        > h3 {
+
+        }
+
+        > span {
+
+        }
+
+        > span {
+
+        }
+    }
+}
+</style>
